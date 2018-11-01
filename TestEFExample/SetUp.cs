4896d6e87c61d4b9566bc47c467e6730bad2dcdd
Ulifer853 <ufernand853@gmail.com>
2018-11-01 11:05:46 -0300

    SetUp to clean data to test

---
+++ TestEFExample/SetUp.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEFExample
+{
+    public class SetUpTest
+    {
+            public void SetupCleanDB()
+            {
+                CleanDB cleaner = new CleanDB();
+                cleaner.EmptyDB();
+            }
+    }
+}
