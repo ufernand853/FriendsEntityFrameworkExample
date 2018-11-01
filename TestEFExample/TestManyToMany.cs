cea0309a4b856223e91926ceb7f7d982553670e5
Ulifer853 <ufernand853@gmail.com>
2018-11-01 11:11:53 -0300

    TestAddUserToAgenda - This method create a user with two agendas and add a new user to one of this agenda

--- TestEFExample/TestManyToMany.cs
+++ TestEFExample/TestManyToMany.cs
@@ -10,9 +10,17 @@ namespace TestEFExample
     [TestClass]
     public class TestManyToMany
     {
+
+
+        public void SetUp()
+        {
+            SetUpTest setUpTest = new SetUpTest();
+            setUpTest.SetupCleanDB();
+        }
         [TestMethod]
         public void TestAddTwoAgendasToOneUser()
         {
+            SetUp();
 
             User user = new User()
             {
@@ -41,6 +49,55 @@ namespace TestEFExample
             IDataAccess<User> dataAccessUser = new UserDataAccess();
             dataAccessUser.Add(user);
 
+        }
+
+
+        [TestMethod]
+        public void TestAddUserToAgenda()
+        {
+
+            SetUp();
+
+            User user = new User()
+            {
+                Age = 24,
+                Name = "Juan Owner two agendas",
+                Agendas = new List<Agenda>()
+
+            };
+
+            Agenda agendaUno = new Agenda()
+            {
+                Name = "Agenda Uno Juan Owner",
+                Contacts = new List<User>()
+            };
+
+            Agenda agendaDos = new Agenda()
+            {
+                Name = "Agenda Dos Juan Owner",
+                Contacts = new List<User>()
+            };
+
+
+            user.Agendas.Add(agendaUno);
+            user.Agendas.Add(agendaDos);
+
+            IDataAccess<User> dataAccessUser = new UserDataAccess();
+            dataAccessUser.Add(user);
+
+
+            User contact3 = new User()
+            {
+                Age = 22,
+                Name = "Boy Contact Added to Agenda Uno",
+                Agendas = new List<Agenda>()
+            };
+
+
+            agendaUno.Contacts.Add(contact3);
+
+            IDataAccess<Agenda> dataAccessAgenda = new AgendaDataAccess();
+            dataAccessAgenda.Modify(agendaUno);
         }
     }
 }
